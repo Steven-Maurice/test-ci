1287f9952ad872d788c47f5c4b6509d306ebc7a4
Steven M <100772049+Steven-Maurice@users.noreply.github.com>
2024-02-06 13:35:15 +0100

    Update test_file.py

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,16 +1,7 @@
-import unittest
+import pytest
 
-# The class that contains the test methods
-class MyTestCase(unittest.TestCase):
-    
-    def test_addition(self):
-        result = 2 + 2
-        self.assertEqual(result, 4)
-    
-    def test_uppercase(self):
-        string = "HELLO"
-        self.assertTrue(string.isupper())
+def test_uppercase(self):
+    string = "HELLO"
+    assert string.isupper()) == True
 
-# Running the tests
-if __name__ == '__main__':
-    unittest.main()
+
